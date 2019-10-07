efeb31d27d15a239879d4a9600d8c2744b7564ba
vique254 <victoronyango051@gmail.com>
2019-10-07 10:32:59 +0300

    create functions for UI and Business logic

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,16 +1,59 @@
-$('.amount').reverse(function(){"##,###.##"});
-var total_amount =function() {
-var sum=0;
-$(".mount").each(function(){
-num = $(this).val().replace(',',''  );
-if(num !=0){
-    sum +=total_amount(num);
-}
-});
-$('.amount').val(sum);
-}
-$('.amount').keyup(function(){
-total_amount();
-alert('Total Amount to be paid is' + " ");
-});
+//Business logic
+//fetch size of pizza
+function getPizzaSize() {
+    return $("#pizza-size")
+      .find(":selected")
+      .val();
+  }
+  //fetch crust of pizza
+  function getCrust() {
+    return $("#pizza-crust")
+      .find(":selected")
+      .val();
+  }
+  //fetch topping of pizza
+  function getToppings() {
+    var toppingList = [];
+    $(".toppings :checked").each(function() {
+      toppingList.push($(this).val());
+    });
+    return toppingList;
+  }
 
+
+
+
+
+
+
+// UI logic
+$("document").ready(
+    function(){
+        $("form").submit(
+            function(){
+                var size = $("#sizee").find(":selected").val();
+                alert(size);
+                }
+        );
+    }
+);
+$("document").ready(
+    function(){
+        $("form").submit(
+            function(){
+                var topping = $("#Toppings").find(":checked").val();
+                alert(topping);
+            }
+        );
+    }
+);
+$("document").ready(
+    function(){
+        $("form").submit(
+            function(){
+                var crust = $("#crust").find(":selected").val();
+                alert(crust);
+                }
+        );
+    }
+);
