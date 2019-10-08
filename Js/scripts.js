515d78e60b00dba5ae6411163dd7891ae16b4378
vique254 <victoronyango051@gmail.com>
2019-10-08 23:50:20 +0300

    make changes to index.html,css and Js

--- Js/scripts.js
+++ Js/scripts.js
@@ -20,87 +20,104 @@ function getPizzaSize() {
     return toppingList;
   }
 
-
+  function area(name, estate) {
+    this.name = name;
+    this.estate = estate;
+  }       
+  function Details(size, crust, Toppings,quantity){
+    this.size = size;
+    this.crust = crust;
+    this.Toppings =Toppings;
+    this.quantity =quantity;
+  }
+  function Total(price, quantity, delivery){
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+    this.total=this.price+this.quantity+this.delivery;
+  }
+  Total.prototype.finalTotal = function(){
+    return this.price * this.quantity + this.delivery;
+  }
 
 
 
 
 
 // UI logic
-$("document").ready(
-    function(){
-        $("form").submit(
-            function(){
-                var size = $("#sizee").find(":selected").val();
-                alert(size);
-                }
-        );
-    }
-);
-$("document").ready(
-    function(){
-        $("form").submit(
-            function(){
-           var topping= $('input[name=radio]:checked').val();
-                alert(topping);
-            }
-        );
-    }
-);
-$("document").ready(
-    function(){
-        $("form").submit(
-            function(){
-                var crust = $("#crust").find(":selected").val();
-                alert(crust);
-                }
-        );
-    }
 
-    function Getvalue(size,toppings,crust,quantity){
-        this.selectedSize=size;
-        this.selectedToppings=toppings;
-        this.selectedCrust=crust;
-        this.selectedQuantity=quantity;
-    }
-    function Total(size,quantity,delivery){
-        this.size=size;
-        this.quantity=quantity;
-        this.delivery=delivery;
-    }
-    Total.prototype.finalTotal =function(){
-        return this.size+this.quantity+this.delivery;
-    }
-    (doocument).ready(function(){
-        $(".allme").click(event).preventDefault(){
-            var selectedCrust=("$crust").val();
-            var selectedSize=("$size").val();
-            var selectedQuantity=parseInt("$nums").val();
-            var selected    Toppings=("$Toppings").val();
-            var selectedDelivery=("$devs").val();
-            var deliveryPrices=[100,0];
-            var pizaPrices=[600,800,1400];
-            var deliveryPrices(InputtedPrice-1);
-            var PizzaPrice(InputtedPrice-1);
+    $(document).ready(function(){
+      $('form').submit(function(event){
+        var size = $("#size").find(":selected").val();
+        alert(size);
+        function priceBySize(){
+          if(size === "small"){
+            return 600 * 1;
+          }
+          else if(size === "medium"){
+            return 800 * 1;
+          }
+          else{
+            return 1100 * 1;
+          }
+        }
+        priceBySize();
+        function priceByCrust() {
+          if(crust === "Crispy") {
+            return 300 * 1;
+          } 
+          else if(crust === "Stuffed") {
+            return 150 * 1;
+          }
+           else{
+            return 200 * 1;
+          };
+        }
+        priceByCrust();
+        function priceByToppings() {
+          if (Toppings === "Pepperoni") {
+            return 300 * 1;
+          } 
+          else if (Toppings === "Bacon") {
+            return 250 * 1;
+          } 
+          else {
+            return 150 * 1;
+          }
+        }
+        priceByToppings();
+        var Toppings= $('input[name=radio]:checked').val();
+        alert(Toppings);
+        var crust = $("#crust").find(":selected").val();
+        alert(crust);
+        event.preventDefault();
+        var delivery =$('#delivery').val();
+        var deliveryPrices = ["yes", "no"];
+        if(deliveryPrices === "yes"){
+          var area=prompt ("Enter your location");
+          alert(delivery + area);
+          return 100;
+        }
+        else{
+          alert('thank you')
+        }
+        
+        
+       var quantity=$("#quantity").val()
+        var deliveryPrices = deliveryPrices[delivery];
+        var newDetails = new Details(size, crust, Toppings,quantity);
+        
+        console.log(newDetails);
+     
+        var newBill = new Total(newDetails.price,newDetails.quantity,newDetails.delivery);
+        alert("your order is " + ", " + newDetails.size+" ," + newDetails.Toppings + ', and ' + newDetails.crust  +' '+ 'pizza'+". click ok to view your bill");
+        alert(Total.finalTotal());
+        console.log(newBill);
+        
+      });
+    });
+  
 
-            Total= new Total()
-
-
-
-
-        });
-    };
-    
-function sizeCalcPrice(size) {
-    if (size === "small") {
-      return sizePrice.small * 1;
-    } 
-    else if (size === "medium") {
-      return sizePrice.medium * 1;
-    } 
-    else {
-      return sizePrice.large * 1;
-    };
-  });
+  
 
     
