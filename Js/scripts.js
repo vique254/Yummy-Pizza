88d54fa9f012b4655799b7f709a299ddc90236ea
Vique254 <54931914+vique254@users.noreply.github.com>
2019-10-14 18:00:30 +0300

    Update scripts.js

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,19 +1,5 @@
-// $(document).ready(function() {
-//     $("#hide1,#show1").click(function() {
-//         $("#show1").toggle();
-//         $("#hide1").toggle();
-//     });
-//     $("#hide2,#show2").click(function() {
-//         $("#show2").toggle();
-//         $("#hide2").toggle();
-//     });
-//     $("#hide3,#show3").click(function() {
-//         $("#show3").toggle();
-//         $("#hide3").toggle();
-//     });
-// });
+
 function Details( size, crust, toppings, quantity){
-  // this.selectedType = type;
   this.selectedSize = size;
   this.selectedCrust = crust;
   this.selectedToppings = toppings;
